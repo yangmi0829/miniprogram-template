4c04e91a957c6d0e8f6e41760dc3c71edf76ee01
yangmi <yangmi0829@vip.qq.com>
2020-03-12 17:03:05 +0800

    '新增二维码工具类'

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,6 +4,7 @@ Page({
       {text:'日期格式化', url:'/pages/dateformat/dateformat'},
       {text:'表单校验', url:'/pages/validate/validate'},
       { text: 'API', url: '/pages/api/api' },
+      { text: '二维码生成', url: '/pages/qrcode/qrcode' },
       { text: '图片压缩', url:'/pages/compress/compress'},
       { text: '图片上传', url:'/pages/upload/upload'},
       { text: '图片组件', url:'/pages/image/image'},
